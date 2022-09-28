49ea09f0bd6311f78eca7ee4fa13d6d7125a47a6
jojope38 <peracchiojoaquin16@gmail.com>
2022-09-28 19:29:49 -0300

    create carousel component

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,18 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import Carousel from '../Components/Carousel'
 export default function HomeScreen() {
+  
   return (
     <View>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
-      <Text>HomeScreen</Text>
+      <Carousel/>
     </View>
   )
 }
