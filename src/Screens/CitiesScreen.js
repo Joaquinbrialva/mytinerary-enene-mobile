f459c8ae87e3f0f17d10f46904eb285045ddc2ba
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-29 13:26:09 -0300

    cities screen

--- src/Screens/CitiesScreen.js
+++ src/Screens/CitiesScreen.js
@@ -1,19 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CityCard from '../Components/CityCard'
 
 export default function CitiesScreen() {
   return (
     <View>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
-      <Text>CitiesScreen</Text>
+      <CityCard />
     </View>
   )
 }
