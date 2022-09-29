652bde41116a3baf9d9fffd93abec3b5f2a788df
jojope38 <peracchiojoaquin16@gmail.com>
2022-09-29 18:51:45 -0300

    add funcionality and styles

--- src/Components/Modals/ModalEdit.js
+++ src/Components/Modals/ModalEdit.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useEditItineraryMutation } from '../../../features/citiesAPI'
 import Swal from 'sweetalert2'
-import { Button, View, Text} from 'react-native'
+import { Button, View, Text, StyleSheet, Alert} from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
 
@@ -35,48 +35,23 @@ export default function ModalEdit({onclose,elemento}) {
 
 
 
-    const handlePrice = (e) =>{
-      setPrice(e.target.value)
-    }
-
-
-    const handleDuration = (e) =>{
-      setDuration(e.target.value)
-    }
-
-    const handleTags =(e)=>{
-      setTags(e.target.value)
-    }
-    const handleName =(e)=>{
-      setName(e.target.value)
-    }
-
+   
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-
+      console.log(edit)
         if(edit.name.length < 5 ){
-          Swal.fire({
-              title:'Name Failed',
-              text:'please verify that the name has more than 5 letters , the name must be a descriptive name for the itinerary'
-          })
+          Alert.alert('please verify that the name has more than 5 letters , the name must be a descriptive name for the itinerary')
+
         }else if(edit.duration > 36){
-          Swal.fire({
-            title:'Duration Failed',
-            text:'the duration cannot be longer than 36 hours'
-        })
+          Alert.alert('the duration cannot be longer than 36 hours')
 
         }
         //mandarlo al controlador update
   editItinerary(edit)
     .unwrap()
     .then(() => {
-      Swal.fire({
-          icon:'success',
-          title:'Edited with success',
-          text:'if you will edit agait thats itinerary, can do it , if finished please press close for the come back the previus page',
-          confirmButtonText:'ok'
-        })
+      Alert.alert('Edited with success')
       })
       .then((error) => {
          console.log(error)
@@ -87,24 +62,98 @@ export default function ModalEdit({onclose,elemento}) {
 
 
   return (
-    <View onPress={handleSubmit}>
-          <Text >edit your itinerary </Text>
-            <Text>Name </Text>
-            <TextInput type='text' onChange={handleName}></TextInput>
+    <View style={styles.container} >
+          <Text style={styles.title} >edit your itinerary </Text>
+            <Text style={styles.fixToText} >Name </Text>
+            <TextInput style={styles.inputs} type='text' onChangeText={name => setName(name)}></TextInput>
 
-            <Text>City: {elemento.cities} </Text>
+            <Text style={styles.fixToText} >City: {elemento.cities} </Text>
 
-            <Text>Price: </Text>
-            <TextInput type='number' onChange={handlePrice} ></TextInput>
-            <Text>Tags:  </Text>
-            <TextInput type='number' onChange={handleTags} ></TextInput>
-            <Text>Duration:</Text>
-            <TextInput type='number' onChange={handleDuration} ></TextInput>
-            <View>
-            <Button title='save' onPress={handleSubmit}>Save</Button>
-            <Button  title='close' onPress={onclose}>Close</Button>
-
+            <Text style={styles.fixToText} >Price: </Text>
+            <TextInput style={styles.inputs} type='number' onChangeText={price =>setPrice(price)} ></TextInput>
+            <Text style={styles.fixToText} >Tags:  </Text>
+            <TextInput style={styles.inputs} type='number' onChangeText={tags => setTags(tags)} ></TextInput>
+            <Text style={styles.fixToText} >Duration:</Text>
+            <TextInput style={styles.inputs} type='number' onChangeText={dur =>setDuration(dur)} ></TextInput>
+            <View style={styles.button}>
+              <View style={styles.textButton}>
+            <Button  title='SAVE' onPress={handleSubmit}>Save</Button>
+              </View>
+              <View style={styles.textButton2}>
+              <Button  title='CLOSE' onPress={onclose}>Close</Button>
+              </View>
             </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    marginLeft:50,
+    marginTop:50,
+    justifyContent:'space-around',
+    alignItems:'center',
+
+    width:300,
+    height:420,
+    borderRadius:10,
+    overflow:'hidden',
+    backgroundColor :'white',
+    borderWidth:1
+  },
+
+
+
+
+  title: {
+    margin:20,
+    textAlign: 'center',
+    textDecorationLine:'underline',
+    fontSize:20,
+    color:'green',
+    fontStyle:'italic',
+  },
+  fixToText: {
+    justifyContent: 'space-between',
+    fontStyle:'italic',
+    marginBottom:8,
+    textAlign:'center',
+    borderBottom:5,
+    fontSize:15
+  },
+
+  inputs:{
+    borderRadius:5,
+    width:200,
+    height:30,
+    color:'black',
+    backgroundColor: '#FFFFFF',
+    borderWidth:0.5,
+
+  },
+  button: {
+    marginTop:20,
+    alignItems: 'center',
+    width:230,
+    justifyContent:'space-between',
+    flexDirection:'row',
+    paddingVertical: 5,
+    paddingHorizontal: 25,
+    marginBottom:20,
+   
+    
+  },
+  textButton: {
+      backgroundColor:'#00CC66',
+      borderRadius: 4,
+      borderWidth:0.5,
+      width:80
+  },
+  textButton2: {
+    backgroundColor:'#FF6666',
+    borderRadius: 4,
+    borderWidth:0.5,  
+    width:80
+},
+
+
+});
