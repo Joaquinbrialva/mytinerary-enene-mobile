8ea9bd531da605dd1a500847c833298f2df44004
jojope38 <peracchiojoaquin16@gmail.com>
2022-09-29 12:22:44 -0300

    add styles and alerts Sign up

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View,Text, Button } from 'react-native'
+import { View,Text, Button ,StyleSheet, Alert } from 'react-native'
 import {useAddUserSignUpMutation} from '../../features/citiesAPI'
 import Swal from 'sweetalert2'
 import { useEffect,useState } from 'react'
@@ -14,29 +14,6 @@ let [email,setMail]=useState()
 let [password,setPassword]=useState()
 let [photo,setPhoto]=useState()
 let [user,setUser]=useState()
-
-////////////////////////////////////////////Functions that execute with the changes in the respect inputs/////////////////////////////////
-
-const handleName = function(e){
-    setName(e.target.value)
-
-}
-const handleLastName = function(e){
-    setLastName(e.target.value)
-
-}
-const handleEmail = function(e){
-    setMail(e.target.value)
-    
-}
-const handlePassword = function(e){
-    setPassword(e.target.value)
-    
-}
-const handlePhoto = function(e){
-    setPhoto(e.target.value)
-    
-}
 
 ////////////////////////////////////////////////// object that will be filled for send in post/////////////////////////////////////
 
@@ -63,33 +40,20 @@ const handleSubmit = function(e){
     e.preventDefault()
 
     if(user.name.length < 2 ){
-        Swal.fire({
-            title:'Name Failed',
-            text:'please verify that the name has more than 2 letters and does not include numbers'
-        })
+      Alert.alert('name need had more of two characters')
 
     }else if(user.lastName.length < 2 ){
-        Swal.fire({
-            title:'Last Name Failed',
-            text:'please verify that the last name has more than 2 letters and does not include numbers'
-        })
+        Alert.alert('please verify that the last name has more than 2 letters and does not include numbers')
+      
     }else if(user.mail.includes('@')===false){
-        Swal.fire({
-            title:'Email Failed',
-            text:'please verify that the email includes @ sign'
-        })
+        Alert.alert('please verify that the email includes @ sign')
 
     }else if(user.password.length < 4){
-        Swal.fire({
-            title:'Password Failed',
-            text:'please verify the password , need have more of 4 characters'
-        })
+        Alert.alert('please verify the password , need have more of 4 characters')
 
     }else if(user.photo.length < 4){
-        Swal.fire({
-            title:'Photo Empty',
-            text:'please verify that the photo exist'
-        })
+        Alert.alert('please verify that the photo exist')
+
 
     }else{
         
@@ -99,13 +63,7 @@ const handleSubmit = function(e){
              .then((error) => {
                 console.log(error)
              })
-
-            Swal.fire({
-                icon:'success',
-                title:'Registred with success',
-                text:'please look and read or create yours itineraries',
-                confirmButtonText:'Do It'
-            })
+             Alert.alert('you are sign up successfully')
 
     }
 
@@ -118,25 +76,92 @@ const handleSubmit = function(e){
 
 
   return (
-    <View >
-        <Text>Please Sign Up</Text>
+    <View  style={styles.container}>
+        <Text style={styles.title}>Please Sign Up</Text>
       <View onSubmit={handleSubmit} >
     <View >
-    <Text >Name</Text>
-    <TextInput type='text' onChange={handleName}></TextInput>
-    <Text >Last name</Text>
-    <TextInput type='text' onChange={handleLastName}></TextInput>
-    <Text >Photo Link</Text>
-    <TextInput type='text' onChange={handlePhoto}></TextInput>
-    <Text >Email</Text>
-    <TextInput type='text' onChange={handleEmail}></TextInput>
-    <Text >Password</Text>
-    <TextInput type='password' onChange={handlePassword}></TextInput>
-        <View  >
-        <Button title="singUp">Sign Up</Button>
+    <Text style={styles.fixToText} >Name</Text>
+    <TextInput style={styles.inputs} type='text' onChangeText={text=>setName(text)}></TextInput>
+    <Text style={styles.fixToText} >Last name</Text>
+    <TextInput style={styles.inputs} type='text' onChangeText={text=>setLastName(text)}></TextInput>
+    <Text style={styles.fixToText} >Photo Link</Text>
+    <TextInput style={styles.inputs} type='text' onChangeText={text=>setPhoto(text)}></TextInput>
+    <Text style={styles.fixToText} >Email</Text>
+    <TextInput style={styles.inputs} type='text' onChangeText={text=>setMail(text)}></TextInput>
+    <Text style={styles.fixToText} >Password</Text>
+    <TextInput style={styles.inputs} type='password' onChangeText={text=>setPassword(text)}></TextInput>
+        <View style={styles.button} >
+        <Button onPress={handleSubmit} title="Sing Up">Sign Up</Button>
         </View>
     </View>
    </View>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      marginLeft:50,
+      marginTop:50,
+      justifyContent:'space between',
+      alignItems:'center',
+      textAlign:'center',
+      width:300,
+      height:500,
+      color:'blue',
+      borderRadius:10,
+      overflow:'hidden',
+      backgroundColor :'white',
+      borderWidth:1
+      
+   
+      
+    },
+    title: {
+      textAlign: 'center',
+      marginTop:25,
+      marginBottom:5,
+      fontSize:25,
+      color:'green',
+      fontStyle:'italic',
+    },
+    fixToText: {
+      justifyContent: 'space-between',
+      fontStyle:'italic',
+      marginTop : 5,
+      marginBottom:8,
+      textAlign:'center',
+      borderBottom:5,
+      fontSize:18
+    },
+  
+    inputs:{
+  
+     
+     
+      borderRadius:5,
+      width:200,
+      height:30,
+      color:'blue',
+      backgroundColor: '#FFFFFF',
+      borderWidth:0.5
+    
+    },
+    button: {
+      marginTop:20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 5,
+      paddingHorizontal: 25,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: '#C0C0C0',
+    },
+    textButton: {
+      color:'black'
+    },
+  
+  
+  });
+  
