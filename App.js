0b83e03076ddb30505c6e108b1811cc868189c71
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-29 13:24:45 -0300

    statusbar auto

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { NavigationContainer } from '@react-navigation/native'
 import Drawer from './src/Navetagion/Drawer'
 import { Provider } from 'react-redux';
 import store from './features/store'
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
-  
 
-  return(
+
+  return (
     <Provider store={store}>
+      <StatusBar style='auto' />
       <NavigationContainer>
         <Drawer />
       </NavigationContainer>
