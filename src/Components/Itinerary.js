080e6a99112c0d32e6f9dbf5ca5c6be4621c4c0a
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-30 15:58:15 -0300

    Itinearary styles

--- src/Components/Itinerary.js
+++ src/Components/Itinerary.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, Image, StyleSheet } from 'react-native'
+import { View, Text, Button, Image, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 import { useRemoveItineraryMutation } from '../../features/citiesAPI'
 import { useGetIdItinerariesQuery } from '../../features/citiesAPI'
@@ -7,7 +7,6 @@ import { useSelector } from 'react-redux'
 const Itinerary = (props) => {
 
     const [deleItiner] = useRemoveItineraryMutation()
-    const id = useSelector(state => state.auth.id)
     const role = useSelector(state => state.auth.role)
 
     const {
@@ -36,67 +35,111 @@ const Itinerary = (props) => {
     }
 
     const styles = StyleSheet.create({
-        logo: {
-            width: 300,
-            height: 150,
+        container: {
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            paddingBottom: 50,
+            paddingTop: 30,
+            borderRadius: 10
         },
+
+        title: {
+            borderRadius: 10,
+            backgroundColor: '#000000',
+        },
+
+        text: {
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            color: 'white'
+        },
+
+        imgContainer: {
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+        img: {
+            width: 300,
+            height: 250,
+            borderRadius: 10
+        },
+
+        profileImg: {
+            width: 80,
+            height: 80,
+            borderRadius: 360
+        },
+
+        profileContainer: {
+            padding: 5
+        },
+
+        itinInfo: {
+            padding: 10,
+        },
+
+        item: {
+            fontSize: 18,
+            padding: 5
+        }
+
     })
 
     const cardItinerary = (data) => {
 
         return (
-            <View>
-                <View>
-                    <Text>{data.name}</Text>
+            <View style={styles.container}>
+                <View style={styles.title}>
+                    <Text style={styles.text}>{data.name}</Text>
                 </View>
-                <View>
+                <View style={styles.profileContainer}>
+                    <View>
+                        <Image
+                            style={styles.profileImg}
+                            source={{ uri: data?.user?.photo }}
+                        />
+                        <View style={styles.profileInfo}>
+                            <Text>{data?.user?.name} {data?.user?.lastName}</Text>
+                            <Text>{data?.user?.mail}</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.imgContainer}>
                     <Image
-                        style={styles.logo}
+                        style={styles.img}
                         source={{
                             uri: data.photo
                         }}
                     />
                 </View>
-                <View>
+                <View style={styles.itinInfo}>
                     <View>
                         <View>
-                            <Image
-                                style={styles.logo}
-                                source={{ uri: data?.user?.photo }}
-                            />
+                            <Text style={styles.item}>💲{data.price}</Text>
                         </View>
                         <View>
-                            <Text>{data?.user?.name} {data?.user?.lastName}</Text>
-                            <Text>{data?.user?.mail}</Text>
-                        </View>
-                    </View>
-                    <View>
-                        <View>
-                            <Text>💲{data.price}</Text>
-                        </View>
-                        <View>
-                            <Text>🕐{data.duration}hr</Text>
+                            <Text style={styles.item}>🕐{data.duration}hr</Text>
                         </View>
                         {/* <View className="data-container">
                             <Like like={data.likes} itinerary={data._id} userId={id} />
                         </View> */}
                         <View>
-                            <Text>Tags:</Text>
+                            <Text style={styles.item}>Tags:</Text>
                             {data.tags.map(tag => <Text>#{tag}</Text>)}
                         </View>
                     </View>
-                    <View>
-                        <Button className='delete-itinerary-button' title='Delete itinerary' onPress={handleDelete} value={data._id}></Button>
-                    </View>
+                    {/* <View>
+                        <Button title='Delete itinerary' onPress={handleDelete} value={data._id}></Button> // IF ADMIN SHOW BUTTON, ELSE NO
+                    </View> */}
                 </View>
             </View>
         )
     }
 
     return (
-        <View>
+        <ScrollView>
             {data.map(e => cardItinerary(e))}
-        </View>
+        </ScrollView>
     );
 }
 
