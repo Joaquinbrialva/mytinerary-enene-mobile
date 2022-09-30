17acc3405058ec2faab278b6711194c943555d33
Joaquin Alvarez <93806772+Joaquinbrialva@users.noreply.github.com>
2022-09-30 14:17:26 -0300

    Revert "Details"

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import Drawer from './src/Navetagion/Drawer'
 import { Provider } from 'react-redux';
 import store from './features/store'
 import { StatusBar } from 'expo-status-bar';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Details from './src/Components/Details';
-import HomeScreen from './src/Screens/HomeScreen';
-import Drawer from './src/Components/Drawer';
 
 export default function App() {
 
-  const Stack = createNativeStackNavigator();
+
 
   return (
     <Provider store={store}>
       <StatusBar style='auto' />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={HomeScreen}>
-          <Stack.Screen name='Drawer' component={Drawer} options={{ headerShown: false }} />
-          <Stack.Screen name="Details" component={Details} />
-        </Stack.Navigator>
+        <Drawer />
       </NavigationContainer>
     </Provider>
   )
