2acf6006c08a9267c14c677b1664628c9d74162c
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-30 13:06:18 -0300

    drawer component created

---
+++ src/Components/Drawer.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import HomeScreen from '../Screens/HomeScreen'
+import CitiesScreen from '../Screens/CitiesScreen'
+import MytinerariesScreen from '../Screens/MytinerariesScreen'
+import Edit from '../Screens/Edit'
+import SignIn from '../Screens/SignIn'
+import SignUp from '../Screens/SignUp'
+import NewCity from '../Screens/NewCity'
+import { useSelector } from 'react-redux'
+import LogOut from '../Components/LogOut'
+
+const DrawerNavigator = createDrawerNavigator();
+
+
+
+export default function Drawer() {
+
+  const user = useSelector(state => state.auth.user)
+  const role = useSelector(state => state.auth.role)
+  const logged = useSelector(state => state.auth.logged)
+
+  const navItems = [
+    {name: "Home", component:HomeScreen},
+    {name: "Cities", component:CitiesScreen},
+    
+  ]
+
+  console.log(role)
+
+  if (role === null) {
+    navItems.push(
+      {name: "SignIn", component:SignIn},
+      {name: "SignUp", component:SignUp}
+      )
+  }
+
+  if(role === 'user'){
+    navItems.push({name: "Itineraries", component:MytinerariesScreen},{name:'logout', component:LogOut})
+  }
+
+  if(role === "admin"){
+    navItems.push(
+      {name: "Itineraries", component:MytinerariesScreen},
+      {name: "Edit", component:Edit},
+      {name: "New City", component:NewCity}
+      )
+  }
+
+
+
+
+
+  const generateBtn = (item) => (
+    <DrawerNavigator.Screen name={ item.name } component={ item.component }/>
+  )
+
+  return (
+    <DrawerNavigator.Navigator>
+      {navItems.map(generateBtn)}
+    </DrawerNavigator.Navigator>
+  )
+}
