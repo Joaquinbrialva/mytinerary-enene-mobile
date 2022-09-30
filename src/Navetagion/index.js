6dcb241183838df7b061be3237676ff2125657a1
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-30 03:50:50 -0300

    index created

---
+++ src/Navetagion/index.js
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Details from '../Components/Details';
+import Drawer from '../Components/Drawer';
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Drawer" component={Drawer} />
+                <Stack.Screen name="Details" component={Details} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MyStack;
