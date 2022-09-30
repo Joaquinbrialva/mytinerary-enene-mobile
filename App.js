42324b017ebf1043d974bf6749bfc9587b931572
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-30 03:49:17 -0300

    implemented Stack

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import Drawer from './src/Navetagion/Drawer'
+import Drawer from './src/Components/Drawer'
 import { Provider } from 'react-redux';
 import store from './features/store'
 import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Details from './src/Components/Details';
+import HomeScreen from './src/Screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-
 
   return (
     <Provider store={store}>
       <StatusBar style='auto' />
       <NavigationContainer>
-        <Drawer />
+        <Stack.Navigator initialRouteName={HomeScreen}>
+          <Stack.Screen name='Drawer' component={Drawer} options={{headerShown: false}}/>
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   )
 }
-
-
